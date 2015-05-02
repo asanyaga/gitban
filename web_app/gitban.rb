ef38e94e5263f4d79b097ac4d3f59c90494b14e1
asanyaga <asa.nyaga@gmail.com>
2015-05-02 15:09:32 +0300

    The first commit

---
+++ web_app/gitban.rb
@@ -0,0 +1,54 @@
+
+require 'sinatra'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'mysql2', host: 'localhost',database: 'gitissues',username:'root',password: 'root')
+
+get '/' do
+  @to_do_issues=Issue.joins(assignee: :stage).where('stages.name=? and issues.state=?', 'READY','open')
+  @in_progress_issues=Issue.joins(assignee: :stage).where('(stages.name=? OR stages.name=?) and issues.state=?' , 'DEV','UI','open')
+  @qa_issues=Issue.joins(assignee: :stage).where('stages.name=? and issues.state=?', 'QA','open')
+  @uat_issues=Issue.joins(assignee: :stage).where('stages.name=? and issues.state=?' , 'UAT','open')
+  @events=Event.order("cast(created_on as datetime) desc").limit(10)
+  erb :board
+end
+
+class Event < ActiveRecord::Base
+end
+
+class Issue < ActiveRecord::Base
+	belongs_to :assignee
+
+  def days_open
+    DateTime.now.mjd-Date.parse(self.created_on).mjd
+  end
+
+  def priority
+    labels_arr=self.labels
+    if labels_arr
+      if labels_arr.include? "Critical"
+        "Critical"
+      elsif labels_arr.include? "High"
+        "High"
+      elsif labels_arr.include? "Medium"
+        "Medium"
+      elsif labels_arr.include? "Low"
+        "Low"
+      else
+        'Medium'
+      end
+    else
+      "Medium"
+    end
+  end
+end
+
+class Assignment < ActiveRecord::Base
+end
+
+class Assignee < ActiveRecord::Base
+	belongs_to :stage
+end
+
+class Stage < ActiveRecord::Base
+end

e51120201e99ebf62faa662707ed925017495aac
asanyaga <asa.nyaga@gmail.com>
2015-05-08 15:49:45 +0300

    A few changes

--- web_app/gitban.rb
+++ web_app/gitban.rb
@@ -5,10 +5,10 @@ require 'active_record'
 ActiveRecord::Base.establish_connection(adapter: 'mysql2', host: 'localhost',database: 'gitissues',username:'root',password: 'root')
 
 get '/' do
-  @to_do_issues=Issue.joins(assignee: :stage).where('stages.name=? and issues.state=?', 'READY','open')
-  @in_progress_issues=Issue.joins(assignee: :stage).where('(stages.name=? OR stages.name=?) and issues.state=?' , 'DEV','UI','open')
-  @qa_issues=Issue.joins(assignee: :stage).where('stages.name=? and issues.state=?', 'QA','open')
-  @uat_issues=Issue.joins(assignee: :stage).where('stages.name=? and issues.state=?' , 'UAT','open')
+  @to_do_issues=Issue.joins(assignee: :stage).where('stages.name=? and issues.state=?', 'READY','open').order("CAST(issues.created_on as DATETIME) DESC")
+  @in_progress_issues=Issue.joins(assignee: :stage).where('(stages.name=? OR stages.name=?) and issues.state=?' , 'DEV','UI','open').order("CAST(issues.created_on as DATETIME) DESC")
+  @qa_issues=Issue.joins(assignee: :stage).where('stages.name=? and issues.state=?', 'QA','open').order("CAST(issues.created_on as DATETIME) DESC")
+  @uat_issues=Issue.joins(assignee: :stage).where('stages.name=? and issues.state=?' , 'UAT','open').order("CAST(issues.created_on as DATETIME) DESC")
   @events=Event.order("cast(created_on as datetime) desc").limit(10)
   erb :board
 end
@@ -21,6 +21,23 @@ class Issue < ActiveRecord::Base
 
   def days_open
     DateTime.now.mjd-Date.parse(self.created_on).mjd
+  end
+
+  def category
+      labels_arr=self.labels
+      if labels_arr
+        if labels_arr.include? "Enhancement"
+          "Enhancement"
+        elsif labels_arr.include? "WB Bug"
+          "WB Bug"
+        elsif labels_arr.include? "Prod Bug"
+          "Prod Bug"
+        else
+          "No Category"
+        end
+      else
+        "No Category"
+      end
   end
 
   def priority
